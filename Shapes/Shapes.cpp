8464d50c3349a7225f9f033512faab7260bbbe16
RamtinKosari <ramtinkosari@gmail.com>
2024-10-12 11:32:41 +0330

    [#REFACTOR] Add Shape Header Files and Methods to Draw Line

---
+++ Shapes/Shapes.cpp
@@ -0,0 +1,13 @@
+# ifndef RK_GRAPHICS_SHAPE
+    # include "Shapes.hpp"
+    /**
+     * @brief Construct a New RKShapes Object
+     */
+    RKShapes::RKShapes() {
+    }
+    /**
+     * @brief Destroy the RKShapes Object
+     */
+    RKShapes::~RKShapes() {
+    }
+# endif // RK_GRAPHICS_SHAPE
---
+++ Shapes/Shapes.hpp
@@ -0,0 +1,39 @@
+# ifndef RK_GRAPHICS_SHAPE
+    /**
+     * @file Shapes.hpp
+     * @author Ramtin Kosari (ramtinkosari@gmail.com)
+     * @brief Shapes Header File
+     * @date 2024-10-08
+     */
+    # define RK_GRAPHICS_SHAPE
+    //-- Check if Library is being Compiled by RKACPB
+    # ifdef RKACPB
+        //-- Check if Line Header File is Configured
+        # ifndef HAS_RKACPB_MODULE_LINE
+            error "Line Header File is Required for RK Graphics Shapes Module"
+        # endif // HAS_RKACPB_MODULE_SHAPECNF
+    # endif // RKACPB
+    //-- Include Line File
+    # ifndef RK_GRAPHICS_LINE
+        # include "Line.hpp"
+    # endif // RK_GRAPHICS_LINE
+    /**
+     * @brief Ramtin Kosari's Shapes Class
+     * @class RKShapes
+     */
+    class RKShapes {
+        public:
+            /**
+             * @brief Construct a New RKShapes Object
+             */
+            RKShapes();
+            /**
+             * @brief Destroy the RKShapes Object
+             */
+            ~RKShapes();
+            /**
+             * @brief Line Object
+             */
+            ;
+    };
+# endif // RK_GRAPHICS_SHAPE
